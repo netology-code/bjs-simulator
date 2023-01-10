b899ca2c8519b582dc625c9fccd2f594476105da
Vladimir Chebukin <vovachebr@mail.ru>
2023-01-10 13:14:32 +0300

    update initial

--- initial.js
+++ initial.js
@@ -1,12 +1,9 @@
 function compareArrays(arr1, arr2) {
-  return arr1.every((el, idx) => arr1.length === arr2.length && el === arr2[idx]);  
+  return // решение
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  return users.
-    filter(user => user.gender === gender).
-    map(user => user.age).
-    reduce((average, userAge, index, array) => average + userAge / array.length, 0);
+  return // решение
 }
 
 module.exports = {
